2113f182f084e8cae285066b8796968d1f129e2c
Bansheeboyb <96832836+Bansheeboyb@users.noreply.github.com>
2022-09-19 11:28:00 -0500

    Update hello.js

--- netlify/edge-functions/hello/hello.js
+++ netlify/edge-functions/hello/hello.js
@@ -1,6 +1,7 @@
 export default async (request) => {
-  const raccoons = 2 * 2 || 2;
-
+  const raccoons = get("count") * 2 || 2;
+  console.log("There are now", raccoons, "raccoons 🦝");
+  set("count", raccoons);
   return new Response(raccoons, {
     headers: { "content-type": "text/html" },
   });
